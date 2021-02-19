7fe4533ab20ae9af7f388001754f7f31daa6c458
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-02-18 21:37:37 -0500

    Build en raiz + Staric file

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const e = require('cors')
 
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static)
 
 
 let persons = [
